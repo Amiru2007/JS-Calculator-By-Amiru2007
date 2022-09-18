14ea0149eda1d5094206f2bae531d03d0fada106
Amiru Weerathunga <amiru.upek.weerathunga@gmail.com>
2022-09-18 21:36:31 +0530

    Initial Commit
    Initial Commit

--- script.js
+++ script.js
@@ -10,7 +10,30 @@ function calculate()
     calculator.display.value = eval(calculator.display.value);
 }
 
+const memory = [];
+var list = memory;
+
+function history() {
+    memory.push(eval(calculator.display.value));
+  var list = memory;
+}
+
+function memo() {
+    document.getElementById("displayHistory").innerHTML = memory;
+}
+
 function back() {
     var value = document.getElementById("display").value;
     document.getElementById("display").value = value.substr(0, value.length - 1);
+}
+
+function clearFields() {
+    document.getElementById("display").value=""
+}
+
+function negate() {
+    num1 = document.getElementById("display").value;
+    num2 = -1;
+    negated = num1 * num2
+    document.getElementById("display").value = negated
 }
