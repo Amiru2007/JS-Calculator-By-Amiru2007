cd3c66a5785be02af41c5926ea3965f84d450479
Amiru Weerathunga <amiru.upek.weerathunga@gmail.com>
2022-09-24 17:47:55 +0530

    Initial Commit
    Initial Commit

--- script.js
+++ script.js
@@ -72,13 +72,10 @@ function cos() {
 }
   
   console.log(getTanFromDegrees(0));
-  // expected output: 0
   
   console.log(getTanFromDegrees(45));
-  // expected output: 0.9999999999999999
   
   console.log(getTanFromDegrees(90));
-  // expected output: 16331239353195370
   
 
 function negate() {
